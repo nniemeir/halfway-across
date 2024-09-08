d47788a686ee0add651e556993a1d088bcc9205d
Jacob Niemeir <nniemeir@protonmail.com>
2024-09-08 09:50:12 -0500

    Audio improvements

--- src/verbs/sleep.cpp
+++ src/verbs/sleep.cpp
@@ -1,9 +1,11 @@
+#include "../../include/audio.h"
 #include "../../include/handling.h"
 #include "../../include/world.h"
 
 void Handling::sleep(MainWindow *mainWindow,
                      Location *location) {
   if (location->getName() == "camp") {
+    sfxPlayer.play("qrc:/audio/sfx/sleep.mp3", sfxPlayer.getdefSfxVol(), 0);
     mainWindow->setDescription(
         "You close your eyes and another night passes.\n"),
         world.advanceDay();
